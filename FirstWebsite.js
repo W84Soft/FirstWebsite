dbcbdd3cc0e90a000e6b8e424df2091c17d5e5d7
Radosław Milczarek <116893317+RadoRadoslaw@users.noreply.github.com>
2023-01-31 19:58:12 +0100

    sad

--- FirstWebsite.js
+++ FirstWebsite.js
@@ -1,3 +1,6 @@
-$(".heading. about").on("click",function(){
-    $(".h2").addClass("show");
+$("#about").on("click",function(){
+    $("#h2 ").addClass("show");
+});
+$("#about").on("click",function(){
+    $(" #graphic").addClass("show");
 });
